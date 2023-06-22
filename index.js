31b9547e7bccd8dce9c463944f89d56553b0d143
unknown <shah@bdgwebdesign.com>
2023-06-22 09:10:00 -0700

    Add goals reducer, add app reducer

--- index.js
+++ index.js
@@ -4,12 +4,38 @@
 function todos(state = [], action) { // state = [] because in the beginning the state might be undefined, so we want it to be an array.
   if (action.type === "ADD_TODO") {
     return state.concat([action.todo]);
+  } else if (action.type === "REMOVE_TODO") {
+    return state.filter((todo) => todo.id !== action.id);
+  } else if (action.type === "TOGGLE_TODO") {
+    // use map so that state doesn't directly modify, to keep this a pure function
+    return state.map((todo) => 
+    // Creates a new object, takes the stuff from todo, except for complete, and assigns it to the opposite of todo.complete
+     todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }));
+  } else {
+    return state;
   }
-
-  // Returns state if action.type doesn't === "ADD_TODO"
-  return state;
 }
 
+function goals(state =[], action) {
+  if (action.type === "ADD_GOAL") {
+    return state.concat([action.goal]);
+  } else if (action.type === "REMOVE_GOAL") {
+    return state.filter((goal) => {
+      goal.id !== action.id;
+    });
+  } else if (action.type === "TOGGLE_GOAL") {
+    state.map((goal) => goal.id !== action.id ? goal : Object.assign({}, goal, { complete: !goal.complete }));
+  } else {
+    return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
 // Library Code
 function createStore(reducer) {
   // Manage state
@@ -51,6 +77,18 @@ store.subscribe(() => {
   console.log('The new state is:', store.getState());
 });
 
+
+// This will update the state now everytime this function is ran
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+});
+
+// Use this if you want to unsubscribe a listener
 const unsubscribe = store.subscribe(() => {
   console.log('The state has changed.');
 });
